3a3d8b73cad8962cbd76a020922a713723c50f20
Thousif <thousifnm@gmail.com>
2025-09-25 13:05:40 +1200

    fixed map on Stationcard and CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import { GoogleMapsProvider } from "./components/GoogleMapsProvider";
 import Home from "./pages/Home";
 import Header from "./components/Header.jsx";
@@ -6,30 +6,10 @@ import Footer from "./components/Footer.jsx";
 import FindStation from "./pages/FindStation";
 import StationDetail from "./pages/StationDetail";
 
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/find-station" element={<FindStation />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 export default function App() {
   return (
     <div className="app-shell">
       <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/find-station" element={<FindStation />} />
-        <Route path="/station/:id" element={<StationDetail />} />
-      </Routes>
       <GoogleMapsProvider>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -37,7 +17,6 @@ export default function App() {
           <Route path="/station/:id" element={<StationDetail />} />
         </Routes>
       </GoogleMapsProvider>
-
 
       <Footer />
     </div>
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -245,7 +245,6 @@ export default function Map({ stations = [] }) {
       {/* Optional filter buttons */}
       {/* {filterButtons} */}
 
-
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={defaultCenter}
